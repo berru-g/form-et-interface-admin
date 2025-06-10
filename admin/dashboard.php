b680ebff4998402a39a611c9f9e38fe0113e7840
berru-g <g.leberruyer@gmail.com>
2025-06-10 14:22:25 +0200

    majV1.1

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -26,15 +26,27 @@ try {
 
 <!DOCTYPE html>
 <html lang="fr">
+    <!-- 
+    ============================================
+       Developed by : https://github.com/berru-g/
+       Project : Interface Admin
+       Version : 1.0.2  | 10/06/2025
+       Licence : The MIT License (MIT)
+       Copyright (c) 2025 Berru
+    ============================================
+-->
 <head>
     <meta charset="UTF-8">
     <title>Admin - Base de données</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
     <h2>
-        <i class="fas fa-inbox"></i>
+        <button id="inboxMenu" class="inbox-icon">
+             <i class="fas fa-inbox"></i>
+        </button>
         Dashboard Admin 
         <a class="logout-btn" href="logout.php">
                 <i class="fa-solid fa-arrow-right-from-bracket"></i>
@@ -92,6 +104,34 @@ try {
             <i class="fas fa-headset"></i>
         </a>
     </div>
+    <script>
+        // MENU via lib sweetalert2
+        const hamburgerMenu = document.querySelector('.inbox-icon');
+
+        hamburgerMenu.addEventListener('click', () => {
+            // Utilisation de SweetAlert pour afficher la fenêtre contextuelle
+            Swal.fire({
+                title: '<span style="color:cornflowerblue;"><a href="#">BDD</a></span>',
+                html: `
+    <ul>
+        <li><a href="#"><i class="fas fa-database"></i> SQL 1</a></li>
+        <li><a href="#"><i class="fas fa-table"></i> SQL 2</a></li>
+        <li><a href="#"><i class="fas fa-server"></i> SQL 3</a></li>
+        <li><a href="#"><i class="fas fa-code"></i> SQL 4</a></li>
+        <li><a href="#"><i class="fas fa-chart-line"></i> SQL 5</a></li>
+        <li><a href="#"><i class="fas fa-headset"></i> Assistance</a></li>
+    </ul>
+`,
+                showCloseButton: true,
+                showConfirmButton: false,
+                customClass: {
+                    popup: 'custom-swal-popup',
+                    closeButton: 'custom-swal-close-button',
+                    content: 'custom-swal-content',
+                }
+            });
+        });
+</script>
     <script src="script.js"></script>
 </body>
 </html>
